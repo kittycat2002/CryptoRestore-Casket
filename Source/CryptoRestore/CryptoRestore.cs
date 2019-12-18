715c40b8962dd66dcbd409f547bb815941056ecd
kittycat2002 <josh.masters.02@gmail.com>
2019-12-17 21:34:07 -0500

    Added Mod Options

--- Source/CryptoRestore/CryptoRestore.cs
+++ Source/CryptoRestore/CryptoRestore.cs
@@ -2,23 +2,70 @@
 using System;
 using System.Linq;
 using Verse;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace CryptoRestore
 {
+    public class CryptoRestoreSettings : ModSettings
+    {
+        public static bool luciAddiction = true;
+        public static int unageRate = 30;
+        public static int fuelRate = 20;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref luciAddiction, "LuciAddiction", true);
+            Scribe_Values.Look(ref unageRate, "UnageRate", 30);
+            Scribe_Values.Look(ref fuelRate, "fuelRate", 20);
+            base.ExposeData();
+        }
+        public static void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled("Luciferium Addiction", ref luciAddiction, "Toggles Luciferium Addiction from use.");
+            listingStandard.Label("Unaging Rate: " + unageRate, -1, "How quickly the Pawn unages when inside the casket.");
+            unageRate = (int)Math.Round(listingStandard.Slider(unageRate, 10, 100));
+            listingStandard.Label("Fuel Consumption Rate: " + fuelRate, -1, "How much fuel the casket uses per year.");
+            fuelRate = (int)Math.Round(listingStandard.Slider(fuelRate, 1, 60));
+            listingStandard.End();
+        }
+    }
+
+    public class CryptoRestoreMod : Mod
+    {
+        CryptoRestoreSettings settings;
+
+        public CryptoRestoreMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<CryptoRestoreSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            CryptoRestoreSettings.DoSettingsWindowContents(inRect);
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "CryptoRestore Caskets";
+        }
+    }
     public class Building_CryptoRestore : Building_CryptosleepCasket
     {
         int cryptoHediffCooldown;
-        int cryptoHediffCooldownBase = GenDate.TicksPerQuadrum / 2;
+        readonly int cryptoHediffCooldownBase = GenDate.TicksPerQuadrum / 2;
         int enterTime;
-        int rate = 30;
-        int fuelConsumption = 20;
-        HediffDef luciAddiDef = HediffDef.Named("LuciferiumAddiction");
-        HediffDef luciDef = HediffDef.Named("LuciferiumHigh");
-        NeedDef luciNeed = DefDatabase<NeedDef>.GetNamed("Chemical_Luciferium", true);
+        readonly HediffDef luciAddiDef = HediffDef.Named("LuciferiumAddiction");
+        readonly HediffDef luciDef = HediffDef.Named("LuciferiumHigh");
+        readonly NeedDef luciNeed = DefDatabase<NeedDef>.GetNamed("Chemical_Luciferium", true);
         CompRefuelable refuelable;
         CompPowerTrader power;
         CompProperties_Power props;
         CompProperties_Refuelable fuelprops;
+
 
         public int AgeHediffs(Pawn pawn)
         {
@@ -54,7 +101,7 @@ namespace CryptoRestore
             power = GetComp<CompPowerTrader>();
             props = power.Props;
             fuelprops = refuelable.Props;
-            fuelprops.fuelConsumptionRate = fuelConsumption / 60f;
+            fuelprops.fuelConsumptionRate = CryptoRestoreSettings.fuelRate / 60f;
         }
 
         public override void ExposeData()
@@ -65,6 +112,7 @@ namespace CryptoRestore
         }
         public override void Tick()
         {
+            fuelprops.fuelConsumptionRate = CryptoRestoreSettings.fuelRate / 60f;
             if (HasAnyContents && refuelable.HasFuel)
             {
                 Pawn pawn = ContainedThing as Pawn;
@@ -74,10 +122,10 @@ namespace CryptoRestore
                     power.PowerOutput = -props.basePowerConsumption;
                     if (power.PowerOn)
                     {
-                        refuelable.ConsumeFuel(fuelConsumption / GenDate.TicksPerYear);
+                        refuelable.ConsumeFuel(CryptoRestoreSettings.fuelRate / GenDate.TicksPerYear);
                         cryptoHediffCooldown = Math.Max(cryptoHediffCooldown - 1, 0);
                         if (pawn.ageTracker.AgeBiologicalTicks > GenDate.TicksPerYear * 21)
-                            pawn.ageTracker.AgeBiologicalTicks = Math.Max(pawn.ageTracker.AgeBiologicalTicks - rate, GenDate.TicksPerYear * 21);
+                            pawn.ageTracker.AgeBiologicalTicks = Math.Max(pawn.ageTracker.AgeBiologicalTicks - CryptoRestoreSettings.unageRate, GenDate.TicksPerYear * 21);
                         if (hasHediffs && cryptoHediffCooldown == 0)
                         {
                             foreach (Hediff oldHediff in pawn.health.hediffSet.GetHediffs<Hediff>().ToList())
@@ -149,11 +197,11 @@ namespace CryptoRestore
         public override void EjectContents()
         {
             Pawn pawn = ContainedThing as Pawn;
-            if (pawn.ageTracker.AgeBiologicalTicks >= GenDate.TicksPerYear * 21)
+            if (CryptoRestoreSettings.luciAddiction && pawn.ageTracker.AgeBiologicalTicks >= GenDate.TicksPerYear * 21)
             {
                 pawn.health.AddHediff(luciDef);
                 pawn.health.AddHediff(luciAddiDef);
-                if (Find.TickManager.TicksGame  - enterTime >= GenDate.TicksPerDay * 3)
+                if (Find.TickManager.TicksGame - enterTime >= GenDate.TicksPerDay * 3)
                     pawn.needs.TryGetNeed(luciNeed).CurLevelPercentage = 1f;
             }
             power.PowerOutput = 0;
@@ -179,8 +227,9 @@ namespace CryptoRestore
             {
                 Pawn pawn = ContainedThing as Pawn;
                 pawn.ageTracker.AgeBiologicalTicks.TicksToPeriod(out int years, out int quadrums, out int days, out float hours);
-                string bioTime = "AgeBiological".Translate(new object[]{years,quadrums,days});
-                return base.GetInspectString() + ", " + AgeHediffs(pawn).ToString() + " Age Hediffs\n" + bioTime;
+                ((long)Math.Ceiling((float)(pawn.ageTracker.AgeBiologicalTicks - GenDate.TicksPerYear * 21) / CryptoRestoreSettings.unageRate)).TicksToPeriod(out int years2, out int quadrums2, out int days2, out float hours2);
+                string bioTime = TranslatorFormattedStringExtensions.Translate("AgeBiological",years,quadrums,days);
+                return base.GetInspectString() + ", " + AgeHediffs(pawn).ToString() + " Age Hediffs\n" + bioTime + "\n" + "Time Remaining: " + years2 + (years2 == 1 ? " year " : " years ") + quadrums2 + (quadrums2 == 1 ? " quadrum " : " quadrums ") + days2 + (days2 == 1 ? " day " : " days ") + Math.Ceiling(hours2) + (Math.Ceiling(hours2) == 1 ? " hour" : " hours");
             }
             else return base.GetInspectString();
         }
